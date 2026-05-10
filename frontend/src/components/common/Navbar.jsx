5e5ad54228c41d8bf105679e44177ff3dd7faa06
shaik pervez <pervezbasha12@gmail.com>
2026-05-10 21:25:06 +0530

    navbar - logo

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import companyLogo from "../../assets/company_logo.svg";
+import companyLogo from "../../assets/company_logo.png";
 
 const Navbar = () => {
   return (
     <header className="w-full bg-white shadow-sm">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-3 flex items-center">
         <a href="#" className="flex items-center gap-3">
-          <img src={companyLogo} alt="Gharsaathi" className="h-10 w-auto" />
+          <img src={companyLogo} alt="Gharsaathi" className="h-12 w-auto" />
           <span className="sr-only">Gharsaathi Domestic Service</span>
         </a>
       </div>
